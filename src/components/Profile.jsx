3c6ddffae1152ee9adcdc594e184f19c02514ff8
Illia <example@gmail.com>
2021-04-04 16:16:12 +0200

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://1.img-dpreview.com/files/p/TS600x400~sample_galleries/0724895971/0009166108.jpg' />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+        <div>
+                New post
+        </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
